64dfc79a0b5dd2c4dcfa76e2c56665e8f434f741
Syeda Mahnoor <syedamahnoor9e@gmail.com>
2026-03-25 14:57:47 +0500

    fix port for deployment

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const authRoutes= require("./routes/auth");
+const authRoutes = require("./routes/auth");
 const taskRoutes = require("./routes/task");
 
 require("dotenv").config();
 
 const app = express()
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -15,7 +15,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
 app.get("/", (req, res) => {
-    res.send("API is running.");
+  res.send("API is running.");
 });
 
 mongoose
